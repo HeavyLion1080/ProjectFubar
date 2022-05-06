ad41fa5408c8db188c7ad2fbe1b5007288a301f5
jsinger4 <jsinger4@oswego.edu>
2022-05-05 22:25:38 -0400

    Added All Possible Result Screens

--- frontEnd/Result.js
+++ frontEnd/Result.js
@@ -8,11 +8,21 @@ class Result
         this.container.appendChild(this.optionButtonsElement);
     }
 
-    displayResults(data)
+    displayResults(text)
+    {
+        const ul = document.createElement('ul');
+        this.container.appendChild(ul);
+        text.forEach(element => {
+            let li = document.createElement('li');
+            li.innerText = element[0] + element[1] + element[2];
+            ul.appendChild(li);
+        });
+    }
+    adminWin(data)
     {
         this.container.style.background = "green";
         const text = [
-            ['RESULTS',"",""],
+            ['The Players Finished with:',"",""],
             ['Remaining Time: ',data[0]," minutes\n\n"],
             ['Adventurer','',''],
             ['Number of Clicks: ',data[1]," clicks"],
@@ -21,13 +31,35 @@ class Result
             ['Number of CLicks: ',data[3],' clicks'],
             ['Wrong Answer: ',data[4],' answers']
         ]
-
-        const ul = document.createElement('ul');
-        this.container.appendChild(ul);
-        text.forEach(element => {
-            let li = document.createElement('li');
-            li.innerText = element[0] + element[1] + element[2];
-            ul.appendChild(li);
-        });
+        this.displayResults(text);
+    }
+    adminLoss(data)
+    {
+        this.container.style.background = 'red';
+        const text = [
+            ['The Players Ran Out of Time',"",""],
+            ['Adventurer','',''],
+            ['Number of Clicks: ',data[1]," clicks"],
+            ['Wrong Answers: ',data[2],' answers\n\n'],
+            ['Scholar','',''],
+            ['Number of CLicks: ',data[3],' clicks'],
+            ['Wrong Answer: ',data[4],' answers']
+        ]
+        this.displayResults(text);
+    }
+    playerWin(data)
+    {
+        this.container.style.background = 'green';
+        const text = [
+            ['Congratulations, you Won','',''],
+            ['Time Remaining: ',data[0]," minutes"]
+        ]
+        this.displayResults(text);
+    }
+    playerLoss()
+    {
+        const text = [
+            ['You Ran Out of Time','','']
+        ]
     }
 }
